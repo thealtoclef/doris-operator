b1a97d7905ad75e18b672b839c0fdafa5a1a59c4
buu.nguyen <buu.nguyen@cake.vn>
2025-04-10 20:07:34 +0700

    ✨ feat(operator): add initContainers support to pod specifications
    - Introduces `InitContainers` field in `CommonSpec` and `BaseSpec` to allow specifying initialization containers.
    - Updates related functions to utilize the new `InitContainers` configuration.
    - Enhances the flexibility and control for container initialization before main application containers start.

--- api/disaggregated/v1/types.go
+++ api/disaggregated/v1/types.go
@@ -182,6 +182,10 @@ type CommonSpec struct {
 	// Multi Secret for pod.
 	// +optional
 	Secrets []Secret `json:"secrets,omitempty"`
+
+	// InitContainers is a list of containers that should be run before the app containers are started.
+	// +optional
+	InitContainers []corev1.Container `json:"initContainers,omitempty"`
 }
 
 type SystemInitialization struct {
